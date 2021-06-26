1d8178c98212afb1872320a50a04491d9927bc34
praveenmethraskar <55995179+praveenmethraskar@users.noreply.github.com>
2021-06-26 17:27:43 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return (
+        <header>
+            <h1>ShapeAPI BootCamp </h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and react.js</h1>
+      <p>
+        This was an amazing bootcamp taken Up by Shaurya sniha We covered
+        everything from scratch including Javascript and react.js
+      </p>
+    </div>
+  );
+}
+
+export default Note;
